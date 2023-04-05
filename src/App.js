f55eb60ed495fcb18f470953059167e3a9f1bc0b
alliejj99 <118407356+alliejj99@users.noreply.github.com>
2023-04-05 22:41:22 +0900

    MUI 기본 사용

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "@mui/material/Button";
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+// import AccessibilityIcon from "@mui/icons-material/Accessibility";
+import { Accessibility } from "@mui/icons-material";
+import { Typography } from "@mui/material";
+import styled from "@emotion/styled";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button size="large" variant="text">
+        Hello
+      </Button>
+      <Button fullWidth variant="contained" startIcon={<AcUnitIcon />}>
+        Hello
+      </Button>
+      <Button variant="outlined" endIcon={<Accessibility />}>
+        Hello
+      </Button>
+      <Button
+        sx={{
+          margin: "10px",
+          color: "white",
+          backgroundColor: "gray",
+          "&:hover": {
+            backgroundColor: "black",
+          },
+          "&:disabled": {
+            color: "black",
+            backgroundColor: "white",
+          },
+        }}
+      >
+        Custom Style 1
+      </Button>
+      <CustomButton>Custom Style 2</CustomButton>
+
+      <Typography variant="h1" gutterBottom>
+        h1. Text
+      </Typography>
     </div>
   );
 }
+
+const CustomButton = styled(Button)({
+  color: "white",
+  backgroundColor: "pink",
+  "&:hover": {
+    backgroundColor: "black",
+  },
+  "&:disabled": {
+    color: "black",
+    backgroundColor: "white",
+  },
+});
 
 export default App;
